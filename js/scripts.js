69b1ae21b057775ef615262dd57aaa8aba218897
albertogg1 <alberto.garc.garc@gmail.com>
2022-08-11 13:36:08 +0200

    cambio v1

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,15 @@
 /* Typewritting effect */
 var i = 0;
-let txt = ["Alberto García", "Web developer"];
+let txt = ["Alberto García", "estudiante"];
 var speed = 60;
 
 function typeWriter() {
-    console.log(txt['0'].length);
     if (i < txt['0'].length) {
         document.getElementById("typewrite").innerHTML += txt['0'].charAt(i);
         i++;
         setTimeout(typeWriter, speed);
     }
+
 }
 
 typeWriter();
